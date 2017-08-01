c96ff7f689865bf6e7124fbd8d5576bc11e9e46d
wei <vito.yanw@gmail.com>
2017-08-01 09:40:03 -0700

    add 7.Rever_Integer.java

---
+++ Easy/7.Reverse_Integer.java
@@ -0,0 +1,19 @@
+Reverse digits of an integer.
+
+Example1: x = 123, return 321
+Example2: x = -123, return -321
+
+Note:
+The input is assumed to be a 32-bit signed integer. Your function should return 0 when the reversed integer overflows.
+
+public class Solution {
+    public int reverse(int x) {
+        long result = 0;
+        while(x != 0)
+        {
+            result = result*10 + x%10;
+            x = x/10;            
+        }
+        return (result >= Integer.MIN_VALUE && result <= Integer.MAX_VALUE) ? (int)result : 0; 
+    }
+}
