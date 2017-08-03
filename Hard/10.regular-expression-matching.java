0d59a3d1b2ed35dbfd5db34c55d897a4a6be3375
wei <vito>
2017-08-03 14:43:40 +0800

    modify 10.regular-expression-matching.java

--- Hard/10.regular-expression-matching.java
+++ Hard/10.regular-expression-matching.java
@@ -32,6 +32,40 @@
  */
 public class Solution {
     public boolean isMatch(String s, String p) {
-        
+        if(s == null || p == null)
+            return false;
+        int sLength = s.length();
+        boolean[][] m = new boolean[s.length() + 1][p.length() + 1];
+
+        m[0][0] = true;
+        for(int i = 1 ; i < s.length(); i++)
+        {
+            if(p[i - 1] == '*')
+            {
+               if(m[0][i-1] || (j > 2 && m[0][i-2])
+                       m[0][i] = true;
+            }
+        }
+
+        for(int i = 1; i < s.length(); i++)
+            for(int j = 1; i < s[0].length(); j++)
+            {
+                if(p.charAt(j - 1) == s.charAt(i -1))
+                    m[i][j] = m[i - 1][j - 1];
+                if(p.charAt(j - 1) == '.')
+                    m[i][j] = m[i-1][j-1];
+                if(p.charAt(j - 1) = '*')
+                {
+                    if(p.charAt(j - 2) != s.charAt(i-1) &&  p.charAt(j - 2) != '.')
+                    {
+                        m[i][j] = m[i][j - 1];
+                    }
+                    else
+                    {
+                        m[i][j] = m[i - 1][j] | m[i][j - 1] | m[i][j - 2];
+                    }
+                }
+            }
+         return m[s.length()][p.length()]
     }
 }
