07eb1ade79bec227abb93d8eea69e159fd65387e
wei <vito.yanw@gmail.com>
2017-08-04 10:07:33 -0700

    add 38.cout-and-say.java

---
+++ Easy/38.count-and-say.java
@@ -0,0 +1,77 @@
+/*
+ * [38] Count and Say
+ *
+ * https://leetcode.com/problems/count-and-say
+ *
+ * algorithms
+ * Easy (34.48%)
+ * Total Accepted:    142.3K
+ * Total Submissions: 412.7K
+ * Testcase Example:  '1'
+ *
+ * The count-and-say sequence is the sequence of integers with the first five
+ * terms as following:
+ * 
+ * 1.     1
+ * 2.     11
+ * 3.     21
+ * 4.     1211
+ * 5.     111221
+ * 
+ * 
+ * 
+ * 1 is read off as "one 1" or 11.
+ * 11 is read off as "two 1s" or 21.
+ * 21 is read off as "one 2, then one 1" or 1211.
+ * 
+ * 
+ * 
+ * Given an integer n, generate the nth term of the count-and-say sequence.
+ * 
+ * 
+ * 
+ * Note: Each term of the sequence of integers will be represented as a
+ * string.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: 1
+ * Output: "1"
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: 4
+ * Output: "1211"
+ * 
+ * 
+ */
+public class Solution {
+    public String countAndSay(int n) {
+        StringBuilder result = new StringBuilder();
+        if(n <= 0)
+            return result.toString();
+        result.append("1");
+        for(int i = 2; i <= n; i++)
+        {
+            StringBuilder tmp = new StringBuilder();
+            int k = 0;
+            while(k < result.length())
+            {
+                
+                int count = 0;
+                char numbChar = result.charAt(k);
+                while(k < result.length() && result.charAt(k) == numbChar)
+                {
+                    count++;
+                    k++;
+                }
+                tmp.append(String.valueOf(count)).append(String.valueOf(numbChar));
+            }       
+            result = tmp;
+        }
+        return result.toString();        
+    }
+}
